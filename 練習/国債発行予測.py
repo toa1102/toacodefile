a6b9515654daff304cad8db9d6834c9097b67d14
githubtoa1102 <toatoauehara@gmail.com>
2021-10-10 17:28:14 +0900

    確定

---
+++ 練習/国債発行予測.py
@@ -0,0 +1,65 @@
+#国債発行予測
+zanndaka = int(input('国債の２０００年の金額を入力してください'))//10
+nenn10nenn = int(input('10年後の金額を入力してください'))//10
+nenn1 = (nenn10nenn - zanndaka) // 10
+nenn10 = zanndaka + nenn1 * 10
+gurahu = '|'
+p = print('')
+print('２０００年 ' + str(gurahu * zanndaka))
+
+print('２００１年 ' + str(gurahu * (zanndaka + nenn1)))
+
+print('２００２年 ' + str(gurahu * (zanndaka + nenn1 * 2)))
+
+print('２００３年 ' + str(gurahu * (zanndaka + nenn1 * 3)))
+
+print('２００４年 ' + str(gurahu * (zanndaka + nenn1 * 4)))
+
+print('２００５年 ' + str(gurahu * (zanndaka + nenn1 * 5)))
+
+print('２００６年 ' + str(gurahu * (zanndaka + nenn1 * 6)))
+
+print('２００７年 ' + str(gurahu * (zanndaka + nenn1 * 7)))
+
+print('２００８年 ' + str(gurahu * (zanndaka + nenn1 * 8)))
+
+print('２００９年 ' + str(gurahu * (zanndaka + nenn1 * 9)))
+
+print('２０１０年 ' + str(gurahu * (zanndaka + nenn1 * 10)))
+
+print('２０１１年 ' + str(gurahu * (zanndaka + nenn1 * 11)))
+
+print('２０１２年 ' + str(gurahu * (zanndaka + nenn1 * 12)))
+
+print('２０１３年 ' + str(gurahu * (zanndaka + nenn1 * 13)))
+
+print('２０１４年 ' + str(gurahu * (zanndaka + nenn1 * 14)))
+print('２０１５年 ' + str(gurahu * (zanndaka + nenn1 * 15)))
+print('２０１６年 ' + str(gurahu * (zanndaka + nenn1 * 16)))
+print('２０１７年 ' + str(gurahu * (zanndaka + nenn1 * 17)))
+print('２０１８年 ' + str(gurahu * (zanndaka + nenn1 * 18)))
+print('２０１９年 ' + str(gurahu * (zanndaka + nenn1 * 19)))
+print('２０２０年 ' + str(gurahu * (zanndaka + nenn1 * 20)))
+print('２０２１年 ' + str(gurahu * (zanndaka + nenn1 * 21)))
+print('２０２２年 ' + str(gurahu * (zanndaka + nenn1 * 22)))
+print('２０２３年 ' + str(gurahu * (zanndaka + nenn1 * 23)))
+print('２０２４年 ' + str(gurahu * (zanndaka + nenn1 * 24)))
+print('２０２５年 ' + str(gurahu * (zanndaka + nenn1 * 25)))
+print('２０２６年 ' + str(gurahu * (zanndaka + nenn1 * 26)))
+print('２０２７年 ' + str(gurahu * (zanndaka + nenn1 * 27)))
+
+print('２０２８年 ' + str(gurahu * (zanndaka + nenn1 * 28)))
+
+print('２０２９年 ' + str(gurahu * (zanndaka + nenn1 * 29)))
+
+print('２０３０年 ' + str(gurahu * (zanndaka + nenn1 * 30)))
+
+print('２０３１年 ' + str(gurahu * (zanndaka + nenn1 * 31)))
+
+print('２０３２年 ' + str(gurahu * (zanndaka + nenn1 * 32)))
+
+print('２０３３年 ' + str(gurahu * (zanndaka + nenn1 * 33)))
+
+print('２０３４年 ' + str(gurahu * (zanndaka + nenn1 * 34)))
+
+owari = input('終わりですenterを押してください')
