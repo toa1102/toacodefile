e1fd7e46f11d031ec87d990a1cc8e1d702d9ee34
toa1102 <toatoauehara@gmail.com>
2023-02-05 21:59:06 +0900

    new

---
+++ 暗号化Vigenere cipher.py
@@ -0,0 +1,17 @@
+
+key = input('Please write key.')
+
+moji = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+x = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25}
+
+number = x[key]
+anngoubunn = ''
+bunn = input()
+a = -1
+print(key)
+for i in range(len(bunn)):
+    a = a + 1
+    z = x[bunn[a]]
+    anngoubunn = str(anngoubunn) + str(moji[z + number])
+print(anngoubunn)
